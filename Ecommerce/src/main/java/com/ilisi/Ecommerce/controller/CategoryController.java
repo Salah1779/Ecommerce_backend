7b99cebf4fefc99e715162f65042499b411634bc
Binta TRAORE <164543037+binta1112@users.noreply.github.com>
2025-04-01 12:35:18 +0200

    definition du controller Categorie

--- Ecommerce/src/main/java/com/ilisi/Ecommerce/controller/CategoryController.java
+++ Ecommerce/src/main/java/com/ilisi/Ecommerce/controller/CategoryController.java
@@ -1,7 +1,54 @@
 package com.ilisi.Ecommerce.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ilisi.Ecommerce.dto.CategoryDTO;
+import com.ilisi.Ecommerce.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        List<CategoryDTO> categories = categoryService.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Integer id) {
+        CategoryDTO category = categoryService.getCategoryById(id);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = categoryService.saveCategory(categoryDTO);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Integer id) {
+        categoryService.deleteCategory(id);
+        return new ResponseEntity<>("Category deleted", HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Integer id, @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(id, categoryDTO);
+        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<CategoryDTO>> searchCategory(@RequestParam String keyword) {
+        List<CategoryDTO> categories = categoryService.searchCategory(keyword);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
 }
